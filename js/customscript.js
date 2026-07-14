1b8e92e5904f7690e92ca9429119880a25f1704b
badrulalambd <badrulalam.ju@gmail.com>
2026-07-15 05:55:20 +0600

    JavaScript file is added to add a border-bottom for the sticky header only once the page starts scrolling and the header is getting sticky.

---
+++ js/customscript.js
@@ -0,0 +1,15 @@
+
+
+
+/*
+ Add border-buttom for the sticky header
+*/
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 20) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
