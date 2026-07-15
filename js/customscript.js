9a0aa881c25f41a5b90bb3c3843fbb1399a233da
badrulalambd <badrulalam.ju@gmail.com>
2026-07-15 20:15:15 +0600

    I have made the design responsive using CSS media query for Mobile and Tablet view

--- js/customscript.js
+++ js/customscript.js
@@ -7,7 +7,7 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 20) {
+    if (window.scrollY > 100) {
         header.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
